a88a2cfe3742d5284255c0d18350e0b61a4c118e
redebron20 <redebron20@gmail.com>
2020-11-10 22:36:26 -0500

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,37 @@
+require 'pry'
+
 # Add your code here
+
+class Dog
+    attr_accessor :name
+
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        save
+    end
+
+    def save
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.clear_all
+        @@all.clear
+    end
+    
+    #binding.pry
+
+    def self.print_all
+        new_arr = []
+        @@all.each do |dog|
+            new_arr << dog.name
+        end
+        new_arr.join
+        
+    end
+end
